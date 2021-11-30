24cc7594a2bb9311405bf104e4b6af6d6d833d22
johni1228 <akirus1228@gmail.com>
2021-11-30 20:09:18 +0900

    chore: Change deploy_contract script for WETH

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,7 +1,11 @@
-const BorealisswapV2Router02 = artifacts.require("BorealisswapV2Router02");
-
+// const BorealisswapV2Router02 = artifacts.require("BorealisswapV2Router02");
+const WETH = artifacts.require('WETH');
 const { config } = require('./migration-config');
 
-module.exports = function (deployer, network) {
-  deployer.deploy(BorealisswapV2Router02, config[network].factoryAddress, config[network].WETHAddress);
-};
+// module.exports = function (deployer, network) {
+//   deployer.deploy(BorealisswapV2Router02, config[network].factoryAddress, config[network].WETHAddress);
+// };
+
+module.exports = function (deploy) {
+  deployer.deploy(WETH);
+}
