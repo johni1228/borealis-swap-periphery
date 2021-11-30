bbb22a5a2c14cbe3c904cc751aa1dd4b204345a9
johni1228 <akirus1228@gmail.com>
2021-11-30 22:21:20 +0900

    chore: Add init_hash_code in borealisswaplibrary.sol

--- migrations/migration-config.js
+++ migrations/migration-config.js
@@ -1,7 +1,7 @@
 const config = {
     'testnet': {
         factoryAddress: '0xa1F5c1C8965703fA992389434994cC949d0cd048',
-        WETHAddress: '0xae13d989dac2f0debff460ac112a837c89baa7cd'
+        WETHAddress: '0x'
     },
     'development': {
         factoryAddress: '0x89b94d481cb58D413e2AaAC1626b05FF413853F6',
