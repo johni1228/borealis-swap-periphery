4e2dd7c4eced71b268c37e4d5ea4d9df9ee3aae7
johni1228 <akirus1228@gmail.com>
2021-11-30 21:32:17 +0900

    chore: Update IBorealisswapV1Exchange.sol

--- contracts/interfaces/V1/IBorealisswapV1Exchange.sol
+++ contracts/interfaces/V1/IBorealisswapV1Exchange.sol
@@ -4,6 +4,6 @@ interface IBorealisswapV1Exchange {
     function balanceOf(address owner) external view returns (uint);
     function transferFrom(address from, address to, uint value) external returns (bool);
     function removeLiquidity(uint, uint, uint, uint) external returns (uint, uint);
-    function tokenToBnbSwapInput(uint, uint, uint) external returns (uint);
+    function tokenToETHSwapInput(uint, uint, uint) external returns (uint);
     function ethToTokenSwapInput(uint, uint) external payable returns (uint);
 }
